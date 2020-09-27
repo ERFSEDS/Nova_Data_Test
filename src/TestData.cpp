f229d1fb3d0b71b9eb07c5c9c06f3886fc371071
Troy Neubauer <troyneubauer@gmail.com>
2020-09-26 22:04:07 -0400

    added basic windows CI script

--- src/TestData.cpp
+++ src/TestData.cpp
@@ -3,7 +3,7 @@
 
 #include "Body.h"
 
-int main(int argc, char** argv){
-   std::cout << "acos of 0.5" << glm::degrees(glm::acos(0.5)) << std::endl;
+int main(int argc, char** argv) {
+   std::cout << "acos of 0.5 = " << glm::degrees(glm::acos(0.5)) << " degrees" << std::endl;
    return EXIT_SUCCESS;
 }
