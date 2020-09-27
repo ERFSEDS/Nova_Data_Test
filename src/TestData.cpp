095673e0fc87b4b55e24a5c91de426a5feb0e5bf
Troy Neubauer <troyneubauer@gmail.com>
2020-09-27 09:30:45 -0400

    Added CLI parser and spdlog

--- src/TestData.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-
-#include "Body.h"
-
-int main(int argc, char** argv) {
-   std::cout << "acos of 0.5 = " << glm::degrees(glm::acos(0.5)) << " degrees" << std::endl;
-   return EXIT_SUCCESS;
-}
